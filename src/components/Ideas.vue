93fb2da384307b42a73f5e57cc17e8b6b0e84b6a
Stephen Michelini <sdmichelini@gmail.com>
2017-06-28 16:48:36 -0400

    Now ideas are rendered in tile rows

--- src/components/Ideas.vue
+++ src/components/Ideas.vue
@@ -13,8 +13,8 @@
         </div>
         <div class="tile is-ancestor">
             <div class="tile is-vertical">
-                <div class="tile">
-                    <div class="tile is-3 is-parent" v-for="idea in ideas">
+                <div class="tile" v-for="row in getRows()">
+                    <div class="tile is-parent is-3" v-for="idea in row">
                         <a class="tile is-child is-info box">
                             <p class="subtitle">{{ idea.title }}</p>
                             <p class="content">
@@ -38,6 +38,29 @@ import { mapState } from 'vuex'
 
 export default {
     name: 'ideas',
+    methods: {
+        getRows: function () {
+            let rows = [ ]
+            rows.push([ ])
+            let curRowIndex = 0
+            let curRow = 0
+
+            const TILES_PER_ROW = 4
+
+            for (let idea of this.ideas) {
+                // Add an idea to the current row
+                rows[curRow].push(idea)
+                ++curRowIndex
+                if (curRowIndex === TILES_PER_ROW) {
+                    curRowIndex = 0
+                    rows.push([])
+                    ++curRow
+                }
+            }
+            console.log(rows)
+            return rows
+        }
+    },
     mounted: function () {
         this.$store.dispatch('LOAD_IDEAS_LIST')
     },
