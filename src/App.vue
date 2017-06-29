4c7d71bbdc370b3fd653cd64aa142605c71f38fa
Stephen Michelini <sdmichelini@gmail.com>
2017-06-29 12:41:45 -0400

    Added Nav

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <nav class='nav has-shadow'>
+      <div class='nav-left'>
+        <router-link to='/' class='nav-item'>
+          Home
+        </router-link>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
